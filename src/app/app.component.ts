40c5f379f7240bd4bbb3b7d4f752a22b5361843d
Petar Metodiev <petar.metodiev@gmail.com>
2018-12-04 23:15:38 +0200

    Implement basic example of all maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  AfterViewInit,
+  ElementRef
+} from '@angular/core';
+
+import {
+  fromEvent,
+  of,
+  Observable
+} from 'rxjs';
+
+import {
+  mergeMap,
+  switchMap,
+  exhaustMap,
+  concatMap,
+  delay,
+  scan,
+  tap,
+  map
+} from 'rxjs/operators';
+
+const occurancesReducer = (acc, curr) => [...acc, curr];
+const getLength = (arr: any[]) => arr.length;
+const log = (label: string) => (what: string) => console.log(label, what);
+
+const ms = 2000;
 
 @Component({
   selector: 'maps-root',
   templateUrl: './app.component.html',
   styles: []
 })
-export class AppComponent {
-  title = 'rxjs-maps-example';
+export class AppComponent implements AfterViewInit {
+
+  mergeMaps$: Observable<number>;
+  switchMaps$: Observable<number>;
+  concatMaps$: Observable<number>;
+  exhaustMaps$: Observable<number>;
+
+  @ViewChild('mergeMapButton') mergeMapButton: ElementRef<HTMLButtonElement>;
+  @ViewChild('switchMapButton') switchMapButton: ElementRef<HTMLButtonElement>;
+  @ViewChild('concatMapButton') concatMapButton: ElementRef<HTMLButtonElement>;
+  @ViewChild('exhaustMapButton') exhaustMapButton: ElementRef<HTMLButtonElement>;
+
+  ngAfterViewInit() {
+    const fakeBackend = (message) => of(message).pipe(
+      delay(ms),
+      tap(log('emit: '))
+    );
+
+    this.mergeMaps$ = fromEvent(this.mergeMapButton.nativeElement, 'click').pipe(
+      mergeMap(() => fakeBackend('mergeMap')),
+      scan(occurancesReducer, []),
+      map(getLength)
+    );
+
+    this.switchMaps$ = fromEvent(this.switchMapButton.nativeElement, 'click').pipe(
+      switchMap(() => fakeBackend('switchMap')),
+      scan(occurancesReducer, []),
+      map(getLength)
+    );
+
+    this.concatMaps$ = fromEvent(this.concatMapButton.nativeElement, 'click').pipe(
+      concatMap(() => fakeBackend('concatMap')),
+      scan(occurancesReducer, []),
+      map(getLength)
+    );
+
+    this.exhaustMaps$ = fromEvent(this.exhaustMapButton.nativeElement, 'click').pipe(
+      exhaustMap(() => fakeBackend('exhaustMap')),
+      scan(occurancesReducer, []),
+      map(getLength)
+    );
+  }
 }
