21ff34fbe3b420e4f803fcf7cfc1cb2e6f4f3a84
Petar Metodiev <petar.metodiev@gmail.com>
2018-12-04 20:05:21 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'maps-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'rxjs-maps-example';
+}
